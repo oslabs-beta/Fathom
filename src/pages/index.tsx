e85be9558f65998e15a142d4c756722ccc2e1d71
Wayland Singh <wmsingh@ucdavis.edu>
2023-05-24 18:53:24 -0400

    login added to header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+import { api } from "~/utils/api";
+import { LoginHeader } from "./components/LoginHeader";
 
 const Home: NextPage = () => {
 
@@ -10,13 +15,12 @@ const Home: NextPage = () => {
         <meta name="description" content="dashboard for all your kubernetes needs" />
       </Head>
 
+      <LoginHeader/>
       <main className="flex min-h-screen flex-col items-center justify-center">
         <h1 className="text-5xl font-extrabold">hello world</h1>
-
       </main>
     </>
   );
 };
 
 export default Home;
-
