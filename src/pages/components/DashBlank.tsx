6ca887015a36f1db50b828af1f0c560310c3baf6
Melissa Armstrong <mkarmstr@gmail.com>
2023-05-25 19:36:47 -0400

    attempted menu icon

--- src/pages/components/DashBlank.tsx
+++ src/pages/components/DashBlank.tsx
@@ -2,6 +2,7 @@ export function DashBlank() {
     return (
         <div className="mt-80 flex-1  font-bold animated-gradient">
             <div className="text-center ">
+                
                 <div className="text-9xl bg-gradient-to-br from-info/50 bg-clip-text text-transparent bg-size-150%">
                     Fathom
                 </div>
--- src/pages/components/LoginHeader.tsx
+++ src/pages/components/LoginHeader.tsx
@@ -1,17 +1,25 @@
-import {signIn, signOut, useSession } from "next-auth/react"
+import { signIn, signOut, useSession } from "next-auth/react"
+import Image from 'next/image';
 
-export const LoginHeader:React.FC = () => {
+export const LoginHeader: React.FC = () => {
   const { data: sessionData } = useSession()
 
   return (
     //this is the component
     <div className="navbar bg-gradient-to-b from-gray-800 to-base-100/60 fixed top-0 left-0 right-0  p-2">
+       {/* <Image
+          src="/src/pages/assets/fathom2-white.png"
+          alt="Fathom icon"
+          width={60}
+          height={60}
+        /> */}
       <div className="ml-2 flex-1 text-3xl font-bold">
-       <span className="ml-2 bg-gradient-to-br from-info to-blue-gray-800 bg-clip-text text-transparent">Fathom </span>
-      </div> 
+        
+        <span className="ml-2 bg-gradient-to-br from-info to-blue-gray-800 bg-clip-text text-transparent">Fathom </span>
+      </div>
       {/* image of the github user profile */}
 
-      {sessionData?.user.image ? <img src={sessionData.user.image} className="w-9 rounded-full "/>: ""}
+      {sessionData?.user.image ? <img src={sessionData.user.image} className="w-9 rounded-full " /> : ""}
 
       {/* sign in/out button */}
       <button
