cda9e85428ac6d7970bd1b84301a78f73b5234fc
Wayland Singh <62313491+waylandsingh@users.noreply.github.com>
2023-06-02 10:32:57 -0400

    Revert "Melissa dev"

--- src/pages/components/Chart.tsx
+++ src/pages/components/Chart.tsx
@@ -4,13 +4,8 @@ import React from 'react';
 // Chart.tsx: components that hold individual charts; built with intention to size charts specifically 
 // May now be refactored into one single chart component, as they are properly auto-sized by Grafana
 
-type ChartProps = {
-    source: string;
-  };
 
-//   valid source? if no, render something else?
-
-export function Chart({ source } : ChartProps) {
+export function Chart({ source }) {
     return <div className="flex max-w-s flex-col gap-4 rounded-xl p-4">
         {/* Flex container with max width, columns, gap between elements, rounded corners, and padding */}
         <iframe
--- src/pages/components/Dashboard.tsx
+++ src/pages/components/Dashboard.tsx
@@ -1,26 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { signIn, signOut, useSession } from "next-auth/react";
 import ChartContainer from './ChartContainer';
 import { DashBlank } from './DashBlank';
 
 interface DashboardProps {
-  initialClusterIP: string;
-  clusterIPArray: Array<any>;
-  refetchClusterIPArray: any;
-  snapshotObj: any;
+  clusterIP: string;
+  snapshotObj: object;
   setSnapshotObj: any;
   dashNum: number;
 }
 
-const Dashboard: React.FC<DashboardProps> = ({ initialClusterIP, clusterIPArray, refetchClusterIPArray, snapshotObj, setSnapshotObj, dashNum }) => {
+const Dashboard: React.FC<DashboardProps> = ({ clusterIP, snapshotObj, setSnapshotObj, dashNum }) => {
   const [currentTimeStamp, setCurrentTimeStamp] = useState('now');
   const { data: sessionData } = useSession();
-  const [currentClusterIP, setCurrentClusterIP] = useState(initialClusterIP);
-  const [ipArray, setipArray] = useState([]);
-
-  const handleTabClick = (ip: string) => {
-    setCurrentClusterIP(ip);
-  };
 
   const handleSnapshotSubmit = (event: React.FormEvent) => {
     event.preventDefault();
@@ -33,72 +25,30 @@ const Dashboard: React.FC<DashboardProps> = ({ initialClusterIP, clusterIPArray,
     console.log('new snapshotObj', snapshotObj);
   };
 
-  const handleDashboardChange = (event: React.ChangeEvent<HTMLSipectipement>) => {
+  const handleDashboardChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     event.preventDefault();
     const changedTimeStamp = event.target.value;
     setCurrentTimeStamp(changedTimeStamp);
   };
 
-
-  // const onlyUserClusters = clusterIPArray ? clusterIPArray.filter(ip => {
-  //   const userInfo = ip.userId
-  //   return (userInfo == sessionData.user.id);
-  // }) : [];
-  
-  // const onlyIPs = onlyUserClusters.map((obj) => {
-  //   let ip = obj.ipAddress;
-  //   return ip;
-  // });
-  
-  // useEffect(()=> {
-  //   console.log(clusterIPArray)
-  //   setCurrentClusterIP(onlyIPs[0]);
-  //   console.log(onlyIPs[0])
-  //   console.log(currentClusterIP)
-  // },[])
-
-  // useEffect(() => {
-  //   setipArray(onlyIPs);
-  // }, [clusterIPArray]);
-
   return (
     <>
-    {/* {currentClusterIP} */}
-    
-      {dashNum === 1 ?
-        (<div className="tabs flex justify-center">
-          {clusterIPArray?.map((obj) => {
-            
-            return (
-              <a
-              key={obj.ipAddress}
-              className={`tab tab-lg tab-lifted ${obj.ipAddress === currentClusterIP ? 'tab-active' : ''}`}
-              onClick={() => handleTabClick(obj.ipAddress)}
-            >
-                {obj.ipAddress}
-              </a>
-            );
-          })
-          }
-        </div>) : ''}
-
-
       <div className="bg-accent/20 rounded-xl p-2 mb-6">
         <div className="flex justify-between ">
           <div className="dropdown dropdown-right ml-2">
-            <label tabIndex={0} className="btn bg-info/10 m-1 ">{dashNum === 1? "Select Dashboard": "Select Snapshot"}</label>
+            <label tabIndex={0} className="btn m-1 bg-info/10">Select Dashboard</label>
             <select
               tabIndex={0}
-              className="dropdown-content w-52 h-8 ml-1 mt-3 "
+              className="dropdown-content menu shadow bg-base-100 rounded-box w-52 text-primary text-sm bg-opacity-70"
               onChange={handleDashboardChange}
             >
-              {dashNum === 2 ? Object.keys(snapshotObj).map(ip => {
-                if (ip !== 'Current')
+              {dashNum === 2 ? Object.keys(snapshotObj).map(el => {
+                if (el !== 'Current')
                   return (
-                    <option value={snapshotObj[ip]}>{ip}</option>
+                    <option value={snapshotObj[el]}>{el}</option>
                   );
-              }) : Object.keys(snapshotObj).map(ip => (
-                <option value={snapshotObj[ip]}>{ip}</option>
+              }) : Object.keys(snapshotObj).map(el => (
+                <option value={snapshotObj[el]}>{el}</option>
               ))}
             </select>
           </div>
@@ -110,14 +60,12 @@ const Dashboard: React.FC<DashboardProps> = ({ initialClusterIP, clusterIPArray,
           ) : ''}
         </div>
 
-
-
         {(dashNum === 2 && Object.keys(snapshotObj).length > 1) ? (
-          <ChartContainer currentClusterIP={currentClusterIP} currentTimeStamp={currentTimeStamp} />
+          <ChartContainer clusterIP={clusterIP} currentTimeStamp={currentTimeStamp} />
         ) : (dashNum === 1 ? (
-          <ChartContainer currentClusterIP={currentClusterIP} currentTimeStamp={currentTimeStamp} />
+          <ChartContainer clusterIP={clusterIP} currentTimeStamp={currentTimeStamp} />
         ) : (
-          <DashBlank />
+          <DashBlank/>
         ))}
       </div>
     </>
