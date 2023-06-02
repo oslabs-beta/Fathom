47edbc677515b4cda9f5c452e945890dc0083ed7
Wayland Singh <62313491+waylandsingh@users.noreply.github.com>
2023-06-02 10:34:00 -0400

    Revert "updated components for multiple cluster tabs functionalithy"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,24 +2,26 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import React from 'react';
 import Dashboard from './components/Dashboard';
+// import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { api } from "~/utils/api";
 import { LoginHeader } from "./components/LoginHeader";
 import { InteractionBar } from "./components/InteractionBar";
 import { useState } from "react";
 import {DashBlankSignedOut} from 'src/pages/components/DashBlank'
+
 const Home: NextPage = () => {
   const { data: sessionData } = useSession()
-  const {data: clusterIPArray, refetch:refetchClusterIPArray} = api.clusterIP.getAll.useQuery();
-  console.log('the cluster IP',clusterIPArray)
- 
-  // current clusterIP !?
+
+  const [clusterIP, setClusterIP] = useState("")
+
+
   // tRPC EXAMPLE START
   // hook example on how to destructure an array of the snapshots,
   // and a refetching function
   const {data: snapshots, refetch:refetchSnaps} = api.snapshot.getAll.useQuery()
-
-  // hook example of new Mutation -
+ 
+  // hook example of new Mutation - 
   // call using  createNewSnapshot.mutate({unixtime:'newval'})
   // hooks into the defined api in api/routers/snapshot.ts
   const createNewSnapshot = api.snapshot.createNew.useMutation({
@@ -28,21 +30,27 @@ const Home: NextPage = () => {
       console.log('successfully created new snapshot')
     }
   });
+
   // handlesubmit helper that uses the createNewSnapshot mutation/hook
-  const checkSnapshot = () => { 
+  const checkSnapshot = () => {  
     // creates new snapshot with that timestamp
     // NOTE: userId is read automatically from the context(see snapshot.ts>createNew)
     const newTimestamp = Date.now()
     createNewSnapshot.mutate({
       unixtime:String(newTimestamp)
     })
+
+
   refetchSnaps() // refetch and display snapshots
   console.log('snaps later',snapshots)
 }
+
   // tRPC EXAMPLE END
-  // refactored snapshotArr (array of objects) to snapshotObj (object) to keep track of our snapshots in our dropdown 
-  // TODO load up snapshotObj from db according to user info 
+
+  // refactored snapshotArr (array of objects) to snapshotObj (object) to keep track of our snapshots in our dropdown  
+  // TODO load up snapshotObj from db according to user info  
   const [snapshotObj, setSnapshotObj] = useState({Current: 'now'})
+
   return (
     <>
       <Head>
@@ -50,22 +58,29 @@ const Home: NextPage = () => {
         <meta name="description" content="dashboard for all your kubernetes needs" />
       </Head>
       {/* extra button to test tRPC hooks */}
+
       {/* can be modified here for components */}
-   
+    
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-gray-800 to-black pt-12">
       {/* this button tests the snapshot procedures: remove when snapshot CRUD is implemented */}
       {/* <button onClick={(e)=>{checkSnapshot()}}>blah</button> */}
+
+        
         <LoginHeader />
+   
        {/* passed in state/setStates as props to components that update/rely on them */}
-        {sessionData?.user.image ? <InteractionBar refetchClusterIPArray={refetchClusterIPArray}/> : <DashBlankSignedOut/>}
+        {sessionData?.user.image ? <InteractionBar clusterIP={clusterIP} setClusterIP={setClusterIP}/> : <DashBlankSignedOut/>}  
+        
         {
-          (sessionData?.user.image && clusterIPArray)
-            ? ( <div> <Dashboard initialClusterIP={clusterIPArray[0]['ipAddress']} refetchClusterIPArray={refetchClusterIPArray} clusterIPArray={clusterIPArray} snapshotObj={snapshotObj} setSnapshotObj={setSnapshotObj} dashNum = {1}/>
-                      <Dashboard initialClusterIP={clusterIPArray[0]['ipAddress']} refetchClusterIPArray={refetchClusterIPArray} clusterIPArray={clusterIPArray} snapshotObj={snapshotObj} setSnapshotObj={setSnapshotObj} dashNum = {2}/>
-                </div>)
+          (sessionData?.user.image && clusterIP) 
+            ? ( <div> <Dashboard clusterIP={clusterIP} snapshotObj={snapshotObj} setSnapshotObj={setSnapshotObj} dashNum = {1}/> 
+                      <Dashboard clusterIP={clusterIP} snapshotObj={snapshotObj} setSnapshotObj={setSnapshotObj} dashNum = {2}/>      
+                </div>) 
             :      ""
         }
       </main>
     </>
   );
 }
+
+export default Home;
