6669f2930719c77544c71ca945a0f7e2961b39e4
sjin-k <sjink@pm.me>
2023-06-05 14:14:26 -0400

    still working on testing clusterIP

--- src/server/api/routers/__tests/clusterIP.test.ts
+++ src/server/api/routers/__tests/clusterIP.test.ts
@@ -1,19 +1,21 @@
-import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { beforeEach, describe, expect, test, it, vi } from 'vitest'
 import type { Session } from 'next-auth'
-import prismaMock from '../../../__mocks__/db'
+import db from '../../../__mocks__/db'
 import { createInnerTRPCContext } from '../../trpc'
 import { clusterIPRouter } from '../clusterIP'
+
 
 // 1- mock prisma module
 vi.mock('../../../db') 
 
-// 2- our tests 
+// 2- our tests
+
 describe('exo procedures testing', () => {
-// 3- Reset everything  
+  // 3- Reset everything  
   beforeEach(() => {
     vi.restoreAllMocks()
   })
-
+  
   // 4- session mocked  
   const session: Session = {
     expires: new Date().toISOString(),
@@ -21,12 +23,18 @@ describe('exo procedures testing', () => {
       id: 'clgb17vnp000008jjere5g15i'
     }
   }
-
+  
   // 5- init tRPC for test
   const ctx = createInnerTRPCContext({ session })
-  const caller = clusterIPRouter.createCaller({ ...ctx, prisma: prismaMock })   
-    
-  describe("procedure 1 - tests", () => {
-    await caller.clusterIP.getAll()
+  const caller = clusterIPRouter.createCaller({ ...ctx, prisma: db })   
+  
+  describe("procedure 1 - tests", async () => {
+    const clusterIP = { id: 'clgb17vnp000008jjere5g15i', ipAddress: '12.34.567.890', userId: 'clgb17vnp000008jjere5g15j', createdAt: new Date(), updatedAt: new Date()}
+    const result = [clusterIP]
+    db.clusterIP.create.mockResolvedValue({...clusterIP})
+    const getAllResult = await caller.clusterIP.getAll()
+    test('getAll clusterIP', () => {
+      expect(getAllResult).toStrictEqual(result)
+    })
   })  
 })
