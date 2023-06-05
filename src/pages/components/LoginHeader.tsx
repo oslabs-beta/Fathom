96cc8a304dcb97f44cee4cdeecef9a39b7aff219
Melissa Armstrong <mkarmstr@gmail.com>
2023-06-05 17:09:01 -0400

    removed unused files, removed unused menu and footer, added favicon

--- src/pages/components/LoginHeader.tsx
+++ src/pages/components/LoginHeader.tsx
@@ -12,19 +12,8 @@ export const LoginHeader: React.FC = () => {
     <div className="navbar bg-gradient-to-br from-gray-800 to-base-100/60 fixed top-0 left-0 right-0  p-1 ">
       {/* gradient background, fixed position, span entire top of page, padding */}
       <div className="navbar-start ">
-        {/* unused menu */}
-        {/* <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost btn-circle ">
-            <svg xmlns="http://www.w3.org/2000/svg" className=" h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-          </label>
-          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>My Clusters</a></li>
-            <li><a>My Snapshots</a></li>
-          </ul>
-        </div> */}
-
+       
       {/* logo */}
-      
       <div className="ml-3 mt-3 flex-1 ">
         <Image
           src={icon}
