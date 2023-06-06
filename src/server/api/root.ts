805801413e058f504dda6dbfbe9e1e2a7a0726be
Melissa Armstrong <mkarmstr@gmail.com>
2023-06-06 18:14:14 -0400

    removed example.ts and references to exampleRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
 import { snapshotRouter } from "~/server/api/routers/snapshot"
 import { clusterIPRouter } from "./routers/clusterIP";
 /**
@@ -8,7 +7,6 @@ import { clusterIPRouter } from "./routers/clusterIP";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   snapshot: snapshotRouter,
   clusterIP: clusterIPRouter,
 });
